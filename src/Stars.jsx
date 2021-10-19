728af4d7c7a2965ec1cf905e8ab89f4be16058b8
KotiMango <rons20000@gmail.com>
2021-10-19 18:03:08 +0300

    Added on star rendering to the average rating

--- src/Stars.jsx
+++ src/Stars.jsx
@@ -1,8 +1,10 @@
-import React, { useState } from 'react';
-
+import React, { useState, useEffect } from 'react';
+import { roundToHalf } from './utils';
 export default function Stars() {
   const [starArray, setStarArray] = useState(new Array(5).fill(0));
   const [ratingArr, setRatingArr] = useState([]);
+  const [isRenderAvg, setIsRenderAvg] = useState(false);
+
   const updateStarArr = (idx, rate) => {
     const fractionRateFlag = starArray.some((star) => star === 0.5);
     const workIdx =
@@ -11,7 +13,6 @@ export default function Stars() {
         : starArray.findIndex((star) => star === 0) !== -1
         ? starArray.findIndex((star) => star === 0)
         : idx;
-    console.log(workIdx);
     if (
       idx !== starArray.lastIndexOf(fractionRateFlag ? 0.5 : 1) &&
       idx !== workIdx
@@ -45,16 +46,24 @@ export default function Stars() {
       }
     }
   };
+
   const currStarRating = starArray.reduce((a, b) => a + b, 0);
   const avgRating =
     Array.isArray(ratingArr) && ratingArr.length
       ? ratingArr.reduce((a, b) => a + b, 0) / ratingArr.length
       : 0;
+
   const submitRate = () => {
+    if (isRenderAvg) {
+      setIsRenderAvg(false);
+      return;
+    }
     setRatingArr((prevState) => [...prevState, currStarRating]);
+    setIsRenderAvg(true);
+    setStarArray(new Array(5).fill(0));
   };
 
-  const starBois = new Array(5)
+  let starBois = new Array(5)
     .fill(0)
     .map((star, idx) => (
       <span
@@ -64,6 +73,40 @@ export default function Stars() {
         key={idx}
       ></span>
     ));
+
+  const renderAvg = () => {
+    const roundedAvg = roundToHalf(avgRating);
+    const avgStarBois = starBois.map((star, idx) => {
+      const starPos = idx;
+      if (roundedAvg > starPos) {
+        if (roundedAvg - starPos === 0.5)
+          return (
+            <span
+              className='star half'
+              aria-hidden='true'
+              key={idx + 10}
+            ></span>
+          );
+        return (
+          <span
+            className='star on'
+            aria-hidden='true'
+            key={idx + 10}
+          ></span>
+        );
+      } else {
+        return (
+          <span
+            className='star'
+            aria-hidden='true'
+            key={idx + 10}
+          ></span>
+        );
+      }
+    });
+    return avgStarBois;
+  };
+  let averageRatingStars = renderAvg();
   return (
     <div
       style={{
@@ -75,11 +118,16 @@ export default function Stars() {
         textAlign: 'center',
       }}
     >
-      <h1>Avg Rating:{avgRating}</h1>
-      {starBois}
-      <div>Rating:{currStarRating}</div>
+      <h1>Avg Rating:{avgRating.toFixed(2)}</h1>
+      {isRenderAvg ? averageRatingStars : starBois}
+      <div>
+        Current Rating:
+        {isRenderAvg
+          ? ' Displaying Average Star Rate'
+          : currStarRating}
+      </div>
       <button className='button' onClick={submitRate}>
-        submit
+        {isRenderAvg ? 'Rate' : 'Submit'}
       </button>
     </div>
   );
