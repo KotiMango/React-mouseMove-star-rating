62c1aee9a7cab52717751ea0f82918802e550c15
KotiMango <rons20000@gmail.com>
2021-10-20 19:33:08 +0300

    Major Refactor Changed Code Logic

--- src/Stars.jsx
+++ src/Stars.jsx
@@ -1,66 +1,35 @@
 import React, { useState } from 'react';
-import {
-  getWorkIdx,
-  renderAvg,
-  getRatingStars,
-  getAvgRating,
-  getStarRating,
-} from './stars-service';
+import { getAvgRating, renderStars } from './stars-service';
 
 export default function Stars() {
-  const [starArray, setStarArray] = useState(new Array(5).fill(0));
   const [ratingArr, setRatingArr] = useState([]);
   const [isRenderAvg, setIsRenderAvg] = useState(false);
+  const [starRate, setStarRate] = useState(0);
 
-  const currStarRating = getStarRating(starArray);
   const avgRating = getAvgRating(ratingArr);
-
-  const updateStarArr = (idx, rate) => {
-    const fractionRateFlag = starArray.some((star) => star === 0.5);
-    const workIdx = getWorkIdx(starArray, idx);
-    if (
-      idx !== starArray.lastIndexOf(fractionRateFlag ? 0.5 : 1) &&
-      idx !== workIdx
-    )
-      return undefined;
-    const starArrCpy = [...starArray];
-    starArrCpy[idx] = rate;
-    if (
-      (idx > 0 && !starArrCpy[idx - 1]) ||
-      starArrCpy[idx + 1] > 0
-    ) {
-      return undefined;
-    } else {
-      setStarArray(starArrCpy);
-      return starArray;
-    }
-  };
 
   const onMouseStar = (ev, idx) => {
     const starCursorPos = ev.nativeEvent.offsetX;
-    if (starCursorPos > 34 && updateStarArr(idx, 1)) {
-      ev.target.classList.remove('half');
-      ev.target.classList.add('on');
-    } else if (starCursorPos < 25 && updateStarArr(idx, 0)) {
-      ev.target.classList.remove('on', 'half');
+    if (starCursorPos > 34) {
+      setStarRate(idx + 1);
+    } else if (starCursorPos < 25) {
+      setStarRate(idx);
     } else {
-      const starArr = updateStarArr(idx, 0.5);
-      if (starArr) {
-        ev.target.classList.add('half');
-        ev.target.classList.remove('on');
-      }
+      setStarRate(idx + 0.5);
     }
   };
 
   const submitRate = () => {
-    if (isRenderAvg) return setIsRenderAvg(false);
-    setRatingArr((prevState) => [...prevState, currStarRating]);
+    if (isRenderAvg) {
+      setStarRate(0);
+      return setIsRenderAvg(false);
+    }
+    setRatingArr((prevState) => [...prevState, starRate]);
     setIsRenderAvg(true);
-    setStarArray(new Array(5).fill(0));
   };
 
-  let ratingStars = getRatingStars(onMouseStar);
-  let averageRatingStars = renderAvg(avgRating);
+  let ratingStars = renderStars(starRate, onMouseStar);
+  let averageRatingStars = renderStars(avgRating);
 
   return (
     <div className='stars-container'>
@@ -69,7 +38,7 @@ export default function Stars() {
         {isRenderAvg ? averageRatingStars : ratingStars}
         <div>
           Current Rating:
-          {isRenderAvg ? ' AVG' : currStarRating}
+          {isRenderAvg ? ' AVG' : starRate}
         </div>
         <button className='button' onClick={submitRate}>
           {isRenderAvg ? 'Rate' : 'Submit'}
